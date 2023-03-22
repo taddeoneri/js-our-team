8955920b1c25de01f4a7d12254a45d41c2a92ef5
taddeoneri <taddeo.neri10@gmail.com>
2023-03-22 16:22:15 +0100

    dati

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+*/
+
+
+const member = [
+    {
+        nome : 'Wayne',
+        surname : 'Barnett',
+        role : 'Founder & CEO',
+        image : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome : 'Angela',
+        surname : 'Caroll',
+        role : 'Chief Editor',
+        image : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome : 'Walter',
+        surname : 'Gordon',
+        role : 'Office Manager',
+        image : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome : 'Angela',
+        surname : 'Lopez',
+        role : 'Social Media Manager',
+        image : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome : 'Scott',
+        surname : 'Estrada',
+        role : 'Developer',
+        image : 'scott-estrada-developer.jpg'
+    },
+    {
+        nome : 'Barbara',
+        surname : 'Ramos',
+        role : 'Graphic Designer',
+        image : 'barbara-ramos-graphic-designer.jpg'
+    }
+]
