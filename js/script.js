dc03f3c8477bbec1c7438c1582aa10fc98305846
taddeoneri <taddeo.neri10@gmail.com>
2023-03-22 16:33:43 +0100

    card

--- js/script.js
+++ js/script.js
@@ -6,39 +6,60 @@ Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
 const member = [
     {
-        nome : 'Wayne',
+        name : 'Wayne',
         surname : 'Barnett',
         role : 'Founder & CEO',
         image : 'wayne-barnett-founder-ceo.jpg'
     },
     {
-        nome : 'Angela',
+        name : 'Angela',
         surname : 'Caroll',
         role : 'Chief Editor',
         image : 'angela-caroll-chief-editor.jpg'
     },
     {
-        nome : 'Walter',
+        name : 'Walter',
         surname : 'Gordon',
         role : 'Office Manager',
         image : 'walter-gordon-office-manager.jpg'
     },
     {
-        nome : 'Angela',
+        name : 'Angela',
         surname : 'Lopez',
         role : 'Social Media Manager',
         image : 'angela-lopez-social-media-manager.jpg'
     },
     {
-        nome : 'Scott',
+        name : 'Scott',
         surname : 'Estrada',
         role : 'Developer',
         image : 'scott-estrada-developer.jpg'
     },
     {
-        nome : 'Barbara',
+        name : 'Barbara',
         surname : 'Ramos',
         role : 'Graphic Designer',
         image : 'barbara-ramos-graphic-designer.jpg'
     }
 ]
+
+let team = '';
+const row = document.querySelector('.row');
+
+for(let i = 0; i < member.length; i++){
+    const memberTeam = 
+    `
+    <div class="col-4">
+        <div class="card"">
+            <img src="./img/${member[i].image}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h4 class="card-text">${member[i].name + ' ' + member[i].surname}</h4>
+                <p class="card-text">${member[i].role}</p>
+            </div>
+        </div>
+    </div>
+    `;
+
+    team += memberTeam;
+}
+row.innerHTML = team;
